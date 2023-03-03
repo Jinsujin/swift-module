a59a615c36967725edd898300ff91d3b2e2b29b4
Jinsujin <sylpid003@gmail.com>
2023-03-03 15:26:26 +0900

    create project

---
+++ LegoWorld/LegoWorld/LegoWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegoWorldApp.swift
+//  LegoWorld
+//
+//  Created by Sujin Jin on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct LegoWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
